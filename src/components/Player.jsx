90145750988cf8df73deb5ddcd235a0a8894410b
Jayesh Nerurkar <jayeshnerurkar.dev@gmail.com>
2024-11-26 12:16:05 +0530

    Solved the time loading error

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -30,8 +30,12 @@ function Player() {
                     <p>{time.currentTime.minute}:{time.currentTime.second}</p>
                     <div onClick={seekSong} ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
                         <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full'/>
-                    </div>
-                    <p>{time.totalTime.minute}:{time.totalTime.second}</p>
+                    </div>              
+                    <p>
+                        {
+                            time.totalTime.minute && time.totalTime.second ? `${time.totalTime.minute}:${time.totalTime.second}`: '0:0'
+                        }
+                    </p>
                 </div>
             </div>
 
